ad4dea52d416d60c14609df96d795b5c2ec4d95a
shironeko2707 <74919082+shironeko2707@users.noreply.github.com>
2021-06-07 14:19:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const {parseDate} = require('chrono-node');
+const {format, parse } = require('date-fns');
+
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -12,17 +15,43 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-
+const isUnixTimestamp = timestamp => {
+  const unixTime = new Date(timestamp * 1000);
+  if (unixTime != 'Invalid Date') {
+    return unixTime
+  } else{
+    return false
+  }
+}
+const isNLDate = timestamp => parseDate(timestamp);
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get('/time', (req, res) => {
+  const response = {
+    unix: null,
+    natural: null
+  }
+  const { timestamp } = req.query
+
+  const parsedTimestamp = isUnixTimestamp(timestamp)
+  const parsedNLDate = isNLDate(timestamp)
+
+  if (parsedTimestamp) {
+    response.unix = parseInt(timestamp, 10),
+    response.natural = format(parsedTimestamp, 'MMMM d, yyyy')
+  }
+
+  if (parsedNLDate) {
+    response.unix = parsedNLDate.getTime()/1000
+    response.natural = timestamp
+  }
+
+  res.send(response)
+})
 
 
 
