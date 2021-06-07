c5636d444b426957675c574dae75434232219eb4
shironeko2707 <74919082+shironeko2707@users.noreply.github.com>
2021-06-07 14:34:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({
+  optionsSuccessStatus: 200
+})); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,9 +23,45 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({
+    greeting: 'hello API'
+  });
 });
 
+app.get("/api/timestamp/", (req, res) => {
+  res.json({
+    unix: Date.now(),
+    utc: Date()
+  });
+});
+
+app.get("/api/timestamp/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
+
+  //A 4 digit number is a valid ISO-8601 for the beginning of that year
+  //5 digits or more must be a unix time, until we reach a year 10,000 problem
+  if (/\d{5,}/.test(dateString)) {
+    const dateInt = parseInt(dateString);
+    //Date regards numbers as unix timestamps, strings are processed differently
+    res.json({
+      unix: dateInt,
+      utc: new Date(dateInt).toUTCString()
+    });
+  } else {
+    let dateObject = new Date(dateString);
+
+    if (dateObject.toString() === "Invalid Date") {
+      res.json({
+        error: "Invalid Date"
+      });
+    } else {
+      res.json({
+        unix: dateObject.valueOf(),
+        utc: dateObject.toUTCString()
+      });
+    }
+  }
+});
 
 
 // listen for requests :)
